52a10649d448ba9cbf92979df87775d070269806
stslex <ilya977.077@gmail.com>
2023-01-07 19:02:15 +0300

    refactor resources

--- app/src/main/java/com/stslex/vpnsec/BaseExt.kt
+++ app/src/main/java/com/stslex/vpnsec/BaseExt.kt
@@ -3,18 +3,21 @@ package com.stslex.vpnsec
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.TRANSPORT_VPN
 
 object BaseExt {
+
     private const val PATH_SETTINGS_VPN = "android.net.vpn.SETTINGS"
 
     val settingsIntent: Intent
         get() = Intent(PATH_SETTINGS_VPN).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
     val Context.isVpnConnect: Boolean
-        get() = with(getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager) {
-            val network = activeNetwork
-            val caps = getNetworkCapabilities(network)
-            caps?.hasTransport(NetworkCapabilities.TRANSPORT_VPN) ?: false
-        }
+        get() = connectiveManager
+            .getNetworkCapabilities(connectiveManager.activeNetwork)
+            ?.hasTransport(TRANSPORT_VPN)
+            ?: false
+
+    private val Context.connectiveManager: ConnectivityManager
+        get() = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 }
